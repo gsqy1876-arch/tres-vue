8205be7c3c29fbcc33dfaee89aec5a88535897fd
cai <gsqy1876@gmial.com>
2026-01-29 14:38:40 +0800

    Remove .nuxt from tracking and update .gitignore

--- .nuxt/dist/server/server.mjs
+++
@@ -1,1 +0,0 @@
-export { default } from "file:///C:/Users/admin/Documents/Workspaces/tres-vue/node_modules/.pnpm/@nuxt+vite-builder@4.3.0_es_a79de7a15eb34a8db7638745215c6596/node_modules/@nuxt/vite-builder/dist/vite-node-entry.mjs"
